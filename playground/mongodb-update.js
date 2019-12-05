dc3c2eca704fbe5ee83456803187adef6090d3b5
asifjirayat <asifjirayat@gmail.com>
2019-12-06 03:11:10 +0530

    Add Update script

---
+++ playground/mongodb-update.js
@@ -0,0 +1,35 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if(err) {
+    return console.log('Unable to connect to MongoDB server.');
+  }
+  console.log('Connected to MongoDB server.')
+  //
+  // db.collection('Todos').findOneAndUpdate({
+  //   _id: new ObjectID('5de97004f185dd4230298382')
+  // }, {
+  //   $set: {
+  //     completed: true
+  //   }
+  // }, {
+  //   returnOriginal: false
+  // }).then((result) => {
+  //   console.log(result);
+  // });
+
+  db.collection('Users').findOneAndUpdate({
+    _id: new ObjectID('5de973d9f185dd42302984f5')
+  }, {
+    $inc: {
+      age: 1
+    }
+  }, {
+    returnOriginal: false
+  }).then((result) => {
+    console.log(result);
+  });
+
+  // db.close();
+});
