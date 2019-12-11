31d74b6e35b8d6997547dd5464c7a81113a31e08
asifjirayat <asifjirayat@gmail.com>
2019-12-12 03:01:29 +0530

    Add DELETE /todos/:id route

--- server/server.js
+++ server/server.js
@@ -37,12 +37,30 @@ app.get('/todos/:id', (req, res) => {
   var id = req.params.id;
 
   if(!ObjectID.isValid(id)) {
-    return res.send().status(404);
+    return res.status(404).send();
   }
 
   Todo.findById(id).then((todo) => {
     if(!todo) {
-      return res.send().status(404);
+      return res.status(404).send();
+    }
+    res.send({todo});
+  }).catch((e) => {
+    res.status(404).send();
+  });
+
+});
+
+app.delete('/todos/:id', (req, res) => {
+  var id = req.params.id;
+
+  if(!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  Todo.findByIdAndRemove(id).then((todo) => {
+    if(!todo) {
+      return res.status(404).send();
     }
     res.send({todo});
   }).catch((e) => {
